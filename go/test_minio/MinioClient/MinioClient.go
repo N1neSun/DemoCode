5426376cb35dbb215c672929b01b20a6b7e4d6aa
N1neSun <917549681@qq.com>
2022-09-01 10:12:40 +0800

    update

--- go/test_minio/MinioClient/MinioClient.go
+++ go/test_minio/MinioClient/MinioClient.go
@@ -5,12 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"mime/multipart"
 
 	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 type MinioClient struct {
-	client *minio.Client
+	Client *minio.Client
 }
 
 const (
@@ -19,74 +21,91 @@ const (
 	contentTypeStream string = "application/octet-stream"
 )
 
-var ctx = context.Background()
+//var ctx = context.Background()
 
-func (c *MinioClient) Createbucket(bucketName string) error {
-	err := c.client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+func (c *MinioClient) Connect() error {
+	endpoint := ""
+	accessKeyID := ""
+	secretAccessKey := ""
+	useSSL := false
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
 	if err != nil {
-		exists, errBucketExists := c.client.BucketExists(context.Background(), bucketName)
+		return errors.New("connect Minio error")
+	}
+	c.Client = minioClient
+	return nil
+}
+
+func (c *MinioClient) Createbucket(ctx context.Context, bucketName string) error {
+	err := c.Client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	if err != nil {
+		exists, errBucketExists := c.Client.BucketExists(context.Background(), bucketName)
 		if errBucketExists == nil && exists {
 			return errors.New("bucketname exists")
 		} else {
-			return errors.New(fmt.Sprintf("Createbucket error, %v", err))
+			return fmt.Errorf("createbucket error, %v", err)
 		}
 	} else {
 		return nil
 	}
 }
 
-func (c *MinioClient) RemoveBucket(bucketName string) error {
-	err := c.client.RemoveBucket(ctx, bucketName)
+func (c *MinioClient) RemoveBucket(ctx context.Context, bucketName string) error {
+	err := c.Client.RemoveBucket(ctx, bucketName)
 	if err != nil {
-		return errors.New(fmt.Sprintf("Remove bucket error, %v", err))
+		return fmt.Errorf("remove bucket error, %v", err)
 	}
 	return nil
 }
 
-func (c *MinioClient) UploadFile(bucketName string, objectName string, filePath string) (string, int64, error) {
-	info, err := c.client.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentTypeFile})
+func (c *MinioClient) UploadFile(ctx context.Context, bucketName string, objectName string, filePath string) (string, int64, error) {
+	info, err := c.Client.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentTypeFile})
 	if err != nil {
-		return "", 0, errors.New(fmt.Sprintf("UploadFile failed, %v", err))
+		return "", 0, fmt.Errorf("uploadFile failed, %v", err)
 	}
 	return objectName, info.Size, nil
 }
 
-func (c *MinioClient) DownloadFile(bucketName string, objectName string, filePath string) error {
-	err := c.client.FGetObject(ctx, bucketName, objectName, filePath, minio.GetObjectOptions{})
+func (c *MinioClient) DownloadFile(ctx context.Context, bucketName string, objectName string, filePath string) error {
+	err := c.Client.FGetObject(ctx, bucketName, objectName, filePath, minio.GetObjectOptions{})
 	if err != nil {
-		return errors.New(fmt.Sprintf("DownloadFile failed, %v", err))
+		return fmt.Errorf("downloadFile failed, %v", err)
 	}
 	return nil
 }
 
-func (c *MinioClient) UploadFileStream(bucketName string, objectName string, fileStream io.Reader, fileSize int64) error {
-	_, err := c.client.PutObject(ctx, bucketName, objectName, fileStream, fileSize, minio.PutObjectOptions{ContentType: contentTypeStream})
+func (c *MinioClient) UploadFileStream(ctx context.Context, bucketName string, objectName string, fileStream multipart.File, fileSize int64) error {
+	_, err := c.Client.PutObject(ctx, bucketName, objectName, fileStream, fileSize, minio.PutObjectOptions{ContentType: contentTypeStream})
 	if err != nil {
-		return errors.New(fmt.Sprintf("UploadFileStream failed, %v", err))
+		return fmt.Errorf("uploadFileStream failed, %v", err)
 	}
 	return nil
 }
 
-func (c *MinioClient) DownloadFileStream(bucketName string, objectName string) (io.Reader, error) {
-	object, err := c.client.GetObject(ctx, bucketName, objectName, minio.GetObjectOptions{})
+func (c *MinioClient) DownloadFileStream(ctx context.Context, bucketName string, objectName string) (io.Reader, error) {
+	object, err := c.Client.GetObject(ctx, bucketName, objectName, minio.GetObjectOptions{})
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("DownloadFileStream failed, %v", err))
+		return nil, fmt.Errorf("downloadFileStream failed, %v", err)
 	}
 	return object, nil
 }
 
-func (c *MinioClient) RemoveFile(bucketName string, objectName string) error {
-	objInfo, err := c.client.StatObject(ctx, bucketName, objectName, minio.StatObjectOptions{})
+func (c *MinioClient) RemoveFile(ctx context.Context, bucketName string, objectName string) error {
+	objInfo, err := c.Client.StatObject(ctx, bucketName, objectName, minio.StatObjectOptions{})
 	if err != nil {
-		return errors.New(fmt.Sprintf("RemoveFile failed, %v", err))
+		return fmt.Errorf("removeFile failed, %v", err)
 	}
 	opts := minio.RemoveObjectOptions{
 		GovernanceBypass: true,
 		VersionID:        objInfo.VersionID,
 	}
-	err = c.client.RemoveObject(ctx, bucketName, objectName, opts)
+	err = c.Client.RemoveObject(ctx, bucketName, objectName, opts)
 	if err != nil {
-		return errors.New(fmt.Sprintf("RemoveFile failed, %v", err))
+		return fmt.Errorf("removeFile failed, %v", err)
 	}
 	return nil
 }
